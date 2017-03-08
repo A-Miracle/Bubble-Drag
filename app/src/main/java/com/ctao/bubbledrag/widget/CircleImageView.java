a8af930b80bfbdaa216dadcb00f679523117d22f
A Miracle <1670562337@qq.com>
2017-03-08 16:55:07 +0800

    update CircleImageView

--- app/src/main/java/com/ctao/bubbledrag/widget/CircleImageView.java
+++ app/src/main/java/com/ctao/bubbledrag/widget/CircleImageView.java
@@ -4,7 +4,6 @@ import android.content.Context;
 import android.graphics.Bitmap;
 import android.graphics.BitmapShader;
 import android.graphics.Canvas;
-import android.graphics.Matrix;
 import android.graphics.Paint;
 import android.graphics.RectF;
 import android.graphics.Shader;
@@ -12,105 +11,97 @@ import android.graphics.drawable.BitmapDrawable;
 import android.graphics.drawable.ColorDrawable;
 import android.graphics.drawable.Drawable;
 import android.util.AttributeSet;
-import android.util.Log;
-import android.widget.ImageView;
 
-public class CircleImageView extends ImageView {
+/**
+ * see https://github.com/A-Miracle/CustomView
+ */
+public class CircleImageView extends android.support.v7.widget.AppCompatImageView {
 
-	protected static final String TAG = CircleImageView.class.getSimpleName();
+    private Paint mBitmapPaint;
+    private float mRadius;
 
-	private Paint mBitmapPaint;
-	private RectF mRect;
-	
-	public CircleImageView(Context context) {
-		this(context, null);
-	}
+    public CircleImageView(Context context) {
+        this(context, null);
+    }
 
-	public CircleImageView(Context context, AttributeSet attrs) {
-		this(context, attrs, 0);
-	}
-	
-	public CircleImageView(Context context, AttributeSet attrs, int defStyleAttr) {
-		super(context, attrs, defStyleAttr);
-		init(context, attrs);
-	}
+    public CircleImageView(Context context, AttributeSet attrs) {
+        this(context, attrs, 0);
+    }
 
-	private void init(Context context, AttributeSet attrs) {
-	}
-	
-	@Override
-	protected void onDraw(Canvas canvas) {
-		if (getDrawable() == null) {
-			return;
-		}
+    public CircleImageView(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+        init(context, attrs);
+    }
 
-		setupRoundePaint();
-		RectF rect = getRectF();
-		int rx = getMeasuredWidth()/2;
-		int ry = getMeasuredHeight()/2;
-		System.out.println("--"+rx+", "+ry);
-		canvas.drawRoundRect(rect, rx, ry, mBitmapPaint);
-	}
-	
-	
-	private void setupRoundePaint() {
-		if (getDrawable() == null) {
-			return;
-		}
-		
-		Bitmap bitmap = getBitmapFromDrawable(getDrawable());
-		if(bitmap == null){
-			return;
-		}
-		
-		BitmapShader bitmapShader = new BitmapShader(bitmap, Shader.TileMode.CLAMP, Shader.TileMode.CLAMP);
+    private void init(Context context, AttributeSet attrs) {
+        mBitmapPaint = new Paint(Paint.ANTI_ALIAS_FLAG);
+        mBitmapPaint.setDither(false);
+    }
 
-		if(getScaleType() != ScaleType.FIT_XY){
-            Log.w(TAG,String.format("Now scale type just support fitXY,other type invalid"));
+    @Override
+    protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
+        super.onMeasure(widthMeasureSpec, heightMeasureSpec);
+        int measuredWidth = getMeasuredWidth();
+        int measuredHeight = getMeasuredHeight();
+        if (measuredWidth != 0 || measuredHeight != 0) {
+            int size = measuredWidth > measuredHeight ? measuredHeight : measuredWidth;
+            mRadius = size / 2.0f;
+            setMeasuredDimension(size, size);
         }
-		
-        //now scale type just support fitXY
-        //todo support all scale type
-        Matrix mMatrix = new Matrix();
-        mMatrix.setScale(getWidth() * 1.0f / bitmap.getWidth(), getHeight() * 1.0f / bitmap.getHeight());
-        bitmapShader.setLocalMatrix(mMatrix);
-        
-		if(mBitmapPaint == null){
-			mBitmapPaint = new Paint(Paint.ANTI_ALIAS_FLAG);
-			mBitmapPaint.setDither(false);
-		}
-        
-		mBitmapPaint.setShader(bitmapShader);
-	}
-	
-	protected RectF getRectF(){
-		if(mRect == null){
-			mRect = new RectF(getPaddingLeft(), getPaddingTop(), getRight() - getLeft() - getPaddingRight(),
-					getBottom() - getTop() - getPaddingBottom());
-		}
-		return mRect;
-	}
-	
-	public static Bitmap getBitmapFromDrawable(Drawable drawable) {
-		if (drawable == null) {
-			return null;
-		}
-		if (drawable instanceof BitmapDrawable) {
-			return ((BitmapDrawable) drawable).getBitmap();
-		}
-		try {
-			Bitmap bitmap;
-			if (drawable instanceof ColorDrawable) {
-				bitmap = Bitmap.createBitmap(1, 1, Bitmap.Config.ARGB_8888);
-			} else {
-				bitmap = Bitmap.createBitmap(drawable.getIntrinsicWidth(), drawable.getIntrinsicHeight(), Bitmap.Config.ARGB_8888);
-			}
-			Canvas canvas = new Canvas(bitmap);
-			drawable.setBounds(0, 0, canvas.getWidth(), canvas.getHeight());
-			drawable.draw(canvas);
-			return bitmap;
-		} catch (OutOfMemoryError e) {
-			return null;
-		}
-	}
+    }
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+        if (!setupBitmapPaint()) {
+            return;
+        }
+        canvas.drawRoundRect(getRectF(), mRadius, mRadius, mBitmapPaint);
+        mBitmapPaint.setShader(null);
+    }
+
+    protected boolean setupBitmapPaint() {
+        Drawable drawable = getDrawable();
+        if (drawable == null) {
+            return false;
+        }
+
+        Bitmap bitmap = getBitmapFromDrawable(drawable);
+        if (bitmap == null) {
+            return false;
+        }
+
+        BitmapShader bitmapShader = new BitmapShader(bitmap, Shader.TileMode.CLAMP, Shader.TileMode.CLAMP);
+        bitmapShader.setLocalMatrix(getImageMatrix());
+        mBitmapPaint.setShader(bitmapShader);
+        return true;
+    }
+
+    protected RectF getRectF() {
+        return new RectF(getPaddingLeft(), getPaddingTop(), getRight() - getLeft() - getPaddingRight(),
+                getBottom() - getTop() - getPaddingBottom());
+    }
+
+    public static Bitmap getBitmapFromDrawable(Drawable drawable) {
+        if (drawable == null) {
+            return null;
+        }
+        if (drawable instanceof BitmapDrawable) {
+            return ((BitmapDrawable) drawable).getBitmap();
+        }
+        try {
+            Bitmap bitmap;
+            if (drawable instanceof ColorDrawable) {
+                bitmap = Bitmap.createBitmap(1, 1, Bitmap.Config.ARGB_8888);
+            } else {
+                bitmap = Bitmap.createBitmap(drawable.getIntrinsicWidth(), drawable.getIntrinsicHeight(),
+                        Bitmap.Config.ARGB_8888);
+            }
+            Canvas canvas = new Canvas(bitmap);
+            drawable.setBounds(0, 0, canvas.getWidth(), canvas.getHeight());
+            drawable.draw(canvas);
+            return bitmap;
+        } catch (OutOfMemoryError e) {
+            return null;
+        }
+    }
 }
